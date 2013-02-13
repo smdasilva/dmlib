04a054be61c2e262f8a2a29bd1f8f849b2c09ad0
smdasilva <smdasilva@wanadoo.fr>
2013-02-13 17:34:30 +0100

    Creation du modele et gestion des metas

--- dmlib/src/dico.java
+++ dmlib/src/dico.java
@@ -1,39 +1,100 @@
-import java.util.Dictionary;
-import java.util.Enumeration;
-import java.util.Hashtable;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.jaudiotagger.audio.exceptions.CannotReadException;
+import org.jaudiotagger.audio.exceptions.InvalidAudioFrameException;
+import org.jaudiotagger.audio.exceptions.ReadOnlyFileException;
+import org.jaudiotagger.tag.TagException;
+
+import com.drew.imaging.ImageProcessingException;
 
 
-public class dico {
-
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-
-			      // create a new hashtable
-			      Dictionary d = new Hashtable();
-			      String[] t = new String[4];
-			      t[0] = "sam";
-			      t[1] = "toto";
-			      d.put(1, t);
-			      System.out.println(t[0]);
-			      String[] f = (String[]) d.get(1);
-			      System.out.println(f[0]);
-			      System.out.println("----------");
-			      //System.out.println(f[0]);
-			      
-			      // add 2 elements
-			      
-			      d.put(4, "Chocolate" + "Bar");
-			      System.out.println("\"1\" is " + d.get(1));
-			      System.out.println("\"4\" is " + d.get(4));
-
-			      // generates a series of elements, one at a time
-			      for (Enumeration e = d.elements(); e.hasMoreElements();) {
-			         System.out.println(e.nextElement());
-			      }
+public class Dico {
+	
+	Map<Integer, String[]> dictionary;
+	Metadata metas;
+	int idIncrement = 0;
 		
-
+	public Dico() {
+		this.dictionary = new HashMap<Integer, String[]>();
+		}
+		
+	public int add(String path, String type, String parent) {
+		String[] datas  = new String[3];
+		datas[0] = path;
+		datas[1] = type;
+		datas[2] = parent;
+		this.dictionary.put(1, datas);
+		return idIncrement++;
 	}
-
+	
+	public String[] getDatas(int id) {
+		return this.dictionary.get(id);
+	}
+	
+	public void remove(int id) {
+		this.dictionary.remove(id);
+	}
+	
+	public ArrayList<String> getChild(String id) {
+		ArrayList<String> result = new ArrayList<String>(); 
+		for (String[] datas : this.dictionary.values()) {
+			String value = datas[2];
+			if (value == id) {
+				result.add(value);
+			}
+		}
+	return result;
+	}
+	
+	public ArrayList<String> getMP3File() {
+		ArrayList<String> result = new ArrayList<String>(); 
+		for (String[] datas : this.dictionary.values()) {
+			String value = datas[2];
+			if (value == "musique") {
+				result.add(value);
+			}
+		}
+		return result;
+	}
+	
+	public ArrayList<String> getJPGFile() {
+		ArrayList<String> result = new ArrayList<String>(); 
+		for (String[] datas : this.dictionary.values()) {
+			String value = datas[2];
+			if (value == "jpg") {
+				result.add(value);
+			}
+		}
+		return result;
+	}
+	
+	public String getPath(int id) {
+		String[] datas = this.getDatas(id);
+		String path = datas[2];
+		return path;
+	}
+	
+	public String getType(int id) {
+		String[] datas = this.getDatas(id);
+		String type = datas[2];
+		return type;
+	}
+	
+	public Mp3Meta getMP3Metas(int id) throws CannotReadException, IOException, TagException, ReadOnlyFileException, InvalidAudioFrameException {
+		File file = new File(this.getPath(id));
+		Mp3Meta metas = new Mp3Meta(file);	
+		return metas;
+	}
+	
+	public JPGMeta getJPGMetas(int id) throws ImageProcessingException, IOException {
+		File file = new File(this.getPath(id));
+		JPGMeta metas = new JPGMeta(file);	
+		return metas;
+	}
+	
+	
 }
