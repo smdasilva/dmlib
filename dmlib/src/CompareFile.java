1dc3b03bafef30cb1b8ec9e5d5616f1020ce783e
smdasilva <smdasilva@wanadoo.fr>
2013-02-19 18:00:11 +0100

    Comparaison finale

--- dmlib/src/CompareFile.java
+++ dmlib/src/CompareFile.java
@@ -14,76 +14,103 @@ public class CompareFile {
 		//boolean resultat = compare("/net/cremi/sdasilva/Documents/Shaun.jpg","/net/cremi/sdasilva/Desktop/photo-dr.jpg");
 		//System.out.println(resultat);
 		
-		byte[]  sortie = checkmd5("/net/cremi/sdasilva/Documents/Shaun.jpg");
+		String  sortie = sha1sum(new File("/net/cremi/sdasilva/Documents/dmlib/dmlib/Caramel.mp3"));
+	    String  sortie2 = sha1sum(new File("/net/cremi/sdasilva/Documents/dmlib/dmlib/Caramel2.mp3"));
 		
-		//convert the byte to hex format
-	    StringBuffer sb = new StringBuffer("");
-	    for (int i = 0; i < sortie.length; i++) {
-	    	sb.append(Integer.toString((sortie[i] & 0xff) + 0x100, 16).substring(1));
-	    }
-	 
-	    System.out.println("Digest(in hex format):: " + sb.toString());
-	    
-	    byte[]  sortie2 = checkmd5("/net/cremi/sdasilva/Desktop/photo-dr.jpg");
-		
-		//convert the byte to hex format
-	    StringBuffer sb2 = new StringBuffer("");
-	    for (int i = 0; i < sortie2.length; i++) {
-	    	sb2.append(Integer.toString((sortie2[i] & 0xff) + 0x100, 16).substring(1));
-	    }
-	 
-	    System.out.println("Digest(in hex format):: " + sb2.toString());
-	
+	    System.out.println(sortie);
+	    System.out.println(sortie2);
 	}
 	
-	public static boolean compare(String f1, String f2) throws NoSuchAlgorithmException, FileNotFoundException {
+	  /** 
+     * compare le <code>sha1sum</code> donné avec l'empreinte du fichier local <code>file</code>. 
+     * @param file le fichier dont on doit calculer l'empreinte sha1 
+     * @param sha1sum l'empreinte à comparer 
+     * @return true si l'empreinte du fichier correspond à <code>sha1sum</code> 
+     */  
+    public static boolean compareSha1sum(File file, String sha1sum) {  
+            boolean res = false;  
+    if (sha1sum.equals(sha1sum(file))) {  
+                    res = true;  
+            }  
+            return res;  
+    }  
 
-		
-		MessageDigest digest = MessageDigest.getInstance("MD5");
-		File file1 = new File(f1);
-		InputStream is1 = new FileInputStream(file1);
-		is1 = new DigestInputStream(is1, digest);
-		byte[] dataToHash1 = digest.digest();
-		
-		
+    /** 
+     * Retourne l'empreinte sha1 de fichier <code>file</code> ou null si le 
+     * fichier n'a pas pu être lu. 
+     * @param file le fichier dont on doit calculer l'empreinte sha1 
+     * @return l'empreinte sha1 
+     */  
+    public static String sha1sum(File file){  
+            String localSha1Sum = null;  
+            if (file.exists() && file.isFile() && file.canRead()){  
+        try {  
+            MessageDigest md = MessageDigest.getInstance("SHA-1");  
+            DigestInputStream dis = new DigestInputStream(new FileInputStream(file), md);  
+            dis.on(true);  
 
-		MessageDigest digest2 = MessageDigest.getInstance("MD5");
-		File file2 = new File(f2);
-		InputStream is2 = new FileInputStream(file2);
-		is2 = new DigestInputStream(is2, digest2);
-		byte[] dataToHash2 = digest2.digest();
+            while (dis.read() != -1){  
+                ;  
+            }  
+            byte[] b = md.digest();  
+            localSha1Sum = getHexString(b);  
+        } catch (Exception ex) {  
+            ex.printStackTrace();  
+        }  
+    } else {  
+        System.out.println("impossible de trouver le fichier : "+file.getAbsolutePath());  
+    }  
+            return localSha1Sum;  
+    }  
+    
+    private static String getHexString(byte[] bytes) {  
+        StringBuilder sb = new StringBuilder(bytes.length*2);  
+        for (byte b : bytes) {  
+                        if (b <= 0x0F && b >= 0x00) { // On rajoute le 0 de poid fort ignoré à la conversion.  
+                                sb.append('0');  
+                        }  
+            sb.append( String.format("%x", b) );  
+        }  
+        return sb.toString();  
+    }
+    
+    
+    
+    public static boolean compareMd5sum(File file, String md5sum) {  
+        boolean res = false;  
+if (md5sum.equals(md5sum(file))){  
+                res = true;  
+        }  
+        return res;  
+}  
 
-		 StringBuffer sb = new StringBuffer("");
-		    for (int i = 0; i < dataToHash1.length; i++) {
-		    	sb.append(Integer.toString((dataToHash1[i] & 0xff) + 0x100, 16).substring(1));
-		    }
-		 
-		  System.out.println("Digest(in hex format):: " + sb.toString());
-		  
-		  
-		  StringBuffer sb2 = new StringBuffer("");
-		    for (int i = 0; i < dataToHash2.length; i++) {
-		    	sb2.append(Integer.toString((dataToHash2[i] & 0xff) + 0x100, 16).substring(1));
-		    }
-		 
-		  System.out.println("Digest(in hex format):: " + sb.toString());
-		
-		return(Arrays.equals(dataToHash1, dataToHash2)); 
+/** 
+ * Retourne l'empreinte md5 de fichier <code>file</code> ou null si le 
+ * fichier n'a pas pu être lu. 
+ * @param file le fichier dont on doit calculer l'empreinte md5 
+ * @return l'empreinte md5 
+ */  
+public static String md5sum(File file) {  
+        String localMd5Sum = null;  
+        if (file.exists() && file.isFile() && file.canRead()){  
+    try {  
+        MessageDigest md = MessageDigest.getInstance("MD5");  
+        DigestInputStream dis = new DigestInputStream(new FileInputStream(file), md);  
+        dis.on(true);  
 
-	}
-	
-	
-	public static byte[] checkmd5(String fichier) throws NoSuchAlgorithmException, IOException {
-		 MessageDigest md = MessageDigest.getInstance("MD5");
-		 InputStream is = new FileInputStream(fichier);
-		 try {
-		   is = new DigestInputStream(is, md);
-		   // read stream to EOF as normal...
-		 }
-		 finally {
-		   is.close();
-		 }
-		 byte[] digest = md.digest();
-		 return digest;
-	 }
+        while (dis.read() != -1){  
+            ;  
+        }  
+        byte[] b = md.digest();  
+        localMd5Sum = getHexString(b);  
+    } catch (Exception ex) {  
+        ex.printStackTrace();  
+    }   
+} else {  
+    System.out.println("impossible de trouver le fichier : "+file.getAbsolutePath());  
+}  
+        return localMd5Sum;  
+}  
+    
 }
+    
