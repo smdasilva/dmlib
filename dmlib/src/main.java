0fa602966cf7c7114b95ee2b436871fa3e7246aa
Rahma <rahma.bendjemaa@yahoo.fr>
2013-02-21 15:18:04 +0100

    s

--- dmlib/src/main.java
+++ dmlib/src/main.java
@@ -12,15 +12,15 @@ public class main {
 	public static void main(String[] args) throws CannotReadException, IOException, TagException, ReadOnlyFileException, InvalidAudioFrameException, CannotWriteException {
 		//ID3Reader id3reader = new ID3Reader("C:/Users/samuel/Desktop/test.MP3");
 		//id3reader.displayinformations();
-		//Mp3Meta mp3 = new Mp3Meta(new File("C:/Users/samuel/Desktop/MP3/test.mp3"));
-		//Mp3Meta mp32 = new Mp3Meta(new File("C:/Users/samuel/Desktop/MP3/test2.mp3"));
-		//mp3.deleteMeta();
-		//mp32.deleteMeta();
+		Mp3Meta mp3 = new Mp3Meta(new File("/net/cremi/rbenojem/espaces/travail/biblio/test.mp3"));
+		Mp3Meta mp32 = new Mp3Meta(new File("/net/cremi/rbenojem/espaces/travail/biblio/test2.mp3"));
+		mp3.deleteMeta();
+		mp32.deleteMeta();
 		//System.out.println(mp3.getTitle());
 		//System.out.println(mp3.getTitle());
 		
-		Mp3Meta mp3 = new Mp3Meta(new File("C:/Users/samuel/Desktop/MP3/test.MP3"));
-		mp3.compareFile(new File("C:/Users/samuel/Desktop/MP3/test2.MP3"));
+		//Mp3Meta mp3 = new Mp3Meta(new File("C:/Users/samuel/Desktop/MP3/test.MP3"));
+		//mp3.compareFile(new File("C:/Users/samuel/Desktop/MP3/test2.MP3"));
 		//mp3.copyTagToFile(new File("C:/Users/samuel/Desktop/MP3/test.mp3"));
 
 	}
