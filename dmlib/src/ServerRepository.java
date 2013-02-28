01d9f310d86dba2270822984c0da95acd4e81c58
Rahma <rahma.bendjemaa@yahoo.fr>
2013-02-28 17:27:48 +0100

    repository

--- dmlib/src/ServerRepository.java
+++ dmlib/src/ServerRepository.java
@@ -1,8 +1,8 @@
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -13,74 +13,114 @@ public class ServerRepository {
 	 * @param args
 	 * @throws IOException
 	 */
-	
-	static File annuaire;
-	static File dir;
-	static File repo;
-	static File repository;
 
-	public void addServer(String server) throws IOException {
-		File dir = new File(
-				"/net/cremi/rbenojem/espaces/travail/dmlib/dmlib/ServerRepository");
-		System.out.println(dir.exists());
-		annuaire = new File(dir, "");
+	public static void addServer(String server) throws IOException {
 
+		File annuaire = new File(
+				"/net/cremi/rbenojem/espaces/travail/dmlib/dmlib/Server");
+		// annuaire.getPath();
+		// System.out.println(annuaire.getPath());
+
+		// System.out.println(annuaire.exists());
 		if (!annuaire.exists()) {
 			// ajout d'un serveur dans l'annuaire
-			new File(dir, "").createNewFile();
-			PrintWriter write;
+			new File("/net/cremi/rbenojem/espaces/travail/dmlib/dmlib/Server")
+					.createNewFile();
 			try {
-				write = new PrintWriter(new FileWriter(annuaire));
-				write.print(server + "\n");
-				write.flush();
-				write.close();
+				BufferedWriter w = new BufferedWriter(
+						new FileWriter(
+								"/net/cremi/rbenojem/espaces/travail/dmlib/dmlib/Server",
+								true));
+				w.write(server + "\n");
+				w.flush();
+				w.close();
 			} catch (NullPointerException a) {
 				System.out.println("Erreur : pointeur null");
 			} catch (IOException a) {
 				System.out.println("Problème d'IO");
 			}
-		}
-
-	}
-
-	public void addRepository(String path) throws IOException {
-		File repo = new File(
-				"/net/cremi/rbenojem/espaces/travail/dmlib/dmlib/ServerRepository");
-		System.out.println(repo.exists());
-		repository = new File(repo, "");
-
-		if (!repository.exists()) {
-			// ecrire le repertoire a synchroniser dans le le fichier
-			// .repository
-			new File(repo, "").createNewFile();
-			PrintWriter write;
-			try {
-				write = new PrintWriter(new FileWriter(repository));
-				write.print(path + "\n");
-				write.flush();
-				write.close();
-			} catch (NullPointerException a) {
-				System.out.println("Erreur : pointeur null");
-			} catch (IOException a) {
-				System.out.println("Problème d'IO");
-			}
-		}
-
-	}
-
-	public List<String> getServer() throws FileNotFoundException {
-		File repos = new File("/net/cremi/rbenojem/espaces/travail/dmlib/dmlib/ServerRepository");
-		
-		File repository = new File(repos, "");
-		if (!repository.exists()) {
-			System.out.println("Annuaire not existe");
 		} else {
-			if (repository.hashCode() == 0) {
-				System.out.println("empty annuaire");
-			} else {
-				List<String> liste = new ArrayList<String>();
 
-				Scanner scanner = new Scanner(new File("/net/cremi/rbenojem/espaces/travail/dmlib/dmlib/ServerRepository"));
+			try {
+				BufferedWriter w = new BufferedWriter(
+						new FileWriter(
+								"/net/cremi/rbenojem/espaces/travail/dmlib/dmlib/Server",
+								true));
+				w.write(server + "\n");
+				w.flush();
+				w.close();
+			} catch (NullPointerException a) {
+				System.out.println("Erreur : pointeur null");
+			} catch (IOException a) {
+				System.out.println("Problème d'IO");
+			}
+
+		}
+
+	}
+
+	public static void addRepository(String path) throws IOException {
+
+		File repository = new File(
+				"/net/cremi/rbenojem/espaces/travail/dmlib/dmlib/Repository");
+
+		if (!repository.exists()) {
+
+			new File(
+					"/net/cremi/rbenojem/espaces/travail/dmlib/dmlib/Repository")
+					.createNewFile();
+			try {
+				BufferedWriter w = new BufferedWriter(
+						new FileWriter(
+								"/net/cremi/rbenojem/espaces/travail/dmlib/dmlib/Repository",
+								true));
+				w.write(path + "\n");
+				w.flush();
+				w.close();
+			} catch (NullPointerException a) {
+				System.out.println("Erreur : pointeur null");
+			} catch (IOException a) {
+				System.out.println("Problème d'IO");
+			}
+
+		} else {
+
+			try {
+				BufferedWriter w = new BufferedWriter(
+						new FileWriter(
+								"/net/cremi/rbenojem/espaces/travail/dmlib/dmlib/Repository",
+								true));
+				w.write(path + "\n");
+				w.flush();
+				w.close();
+			} catch (NullPointerException a) {
+				System.out.println("Erreur : pointeur null");
+			} catch (IOException a) {
+				System.out.println("Problème d'IO");
+			}
+
+		}
+
+	}
+
+	public static List<String> getServer() throws FileNotFoundException {
+		File dir = new File(
+				"/net/cremi/rbenojem/espaces/travail/dmlib/dmlib/Server");
+		List<String> liste = new ArrayList<String>();
+
+		File annuaire = new File(dir, "");
+		if (!annuaire.exists()) {
+			System.out.println("Annuaire not existe");
+			liste = null;
+		} else {
+			if (annuaire.hashCode() == 0) {
+				System.out.println("empty annuaire");
+				liste = null;
+			} else {
+
+				Scanner scanner = new Scanner(
+						new File(
+								"/net/cremi/rbenojem/espaces/travail/dmlib/dmlib/Server"));
 
 				while (scanner.hasNextLine()) {
 					String line = scanner.nextLine();
@@ -90,21 +130,41 @@ public class ServerRepository {
 			}
 
 		}
-		return null;
+		return liste;
 
 	}
 
-	
-	public List<String> getReporitory() throws FileNotFoundException {
-		
-		return null;
-		
+	public static List<String> getRepository() throws FileNotFoundException {
+
+		File repository = new File(
+				"/net/cremi/rbenojem/espaces/travail/dmlib/dmlib/Repository");
+		List<String> liste = new ArrayList<String>();
+
+		// File repository = new File(dir, "");
+		if (!repository.exists()) {
+			System.out.println("Repository not existe");
+			liste = null;
+		} else {
+			if (repository.hashCode() == 0) {
+				System.out.println("empty repository");
+				liste = null;
+			} else {
+
+				Scanner scanner = new Scanner(
+						new File(
+								"/net/cremi/rbenojem/espaces/travail/dmlib/dmlib/Repository"));
+
+				while (scanner.hasNextLine()) {
+					String line = scanner.nextLine();
+					liste.add(line);
+				}
+				scanner.close();
+			}
+
+		}
+		return liste;
+
 	}
-	
-	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		//System.out.println(dir.exists());
-	}
+
 
 }
