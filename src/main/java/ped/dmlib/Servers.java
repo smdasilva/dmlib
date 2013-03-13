a49c6b2f22010459819280b163dffaacae2b7dfd
bnoleau <bnoleau@emi.u-bordeaux1.fr>
2013-03-13 17:04:32 +0100

    toto

---
+++ src/main/java/ped/dmlib/Servers.java
@@ -0,0 +1,35 @@
+package ped.dmlib;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Servers {
+
+	String repositoryPath;
+	Map<String, String> servers = new HashMap<String, String>();
+
+	public Servers(String repositoryPath) {
+		this.repositoryPath = repositoryPath;
+	}
+	
+	public void addServer(String name, String URI){
+		servers.put(name, URI);
+	}
+	
+	public void removeServer(String name){
+		servers.remove(name);
+	}
+	
+	public void existServer(String name){
+		
+	}
+	
+	public void save(){
+		
+	}
+	
+	public String[] getServers(){
+		
+		return null;
+	}
+}
