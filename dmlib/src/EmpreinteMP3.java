d0bdc54b3f8aec6d81aa2cd4f3497b56d3a05dcd
Rahma <rahma.bendjemaa@yahoo.fr>
2013-03-26 14:57:19 +0100

    empreinte MP3

--- dmlib/src/EmpreinteMP3.java
+++ dmlib/src/EmpreinteMP3.java
@@ -1,35 +1,32 @@
-
 import java.util.Scanner;
-
-
 
 public class EmpreinteMP3 {
 
-	public String empreinteMP3(String file) throws Exception{
-	
-		
+	public String empreinteMP3(String file) throws Exception {
+
 		Runtime runtime = Runtime.getRuntime();
-		String cmd[] = new String[]{"./fpcalc", file};
-        final Process process = runtime.exec(cmd);
-        process.waitFor();
-              
-        Scanner out = new Scanner(process.getInputStream());
-        out.next();
-        out.next();
-        
-        String res = null;
-        while (out.hasNextLine())
-            res+=out.nextLine();
-        out.close();
-        return res;
-        
+		String cmd[] = new String[] { "./fpcalc", file };
+		final Process process = runtime.exec(cmd);
+		process.waitFor();
+
+		Scanner out = new Scanner(process.getInputStream());
+		out.next();
+		out.next();
+
+		String res = null;
+		while (out.hasNextLine())
+			res += out.nextLine();
+		out.close();
+		return res;
+
 	}
 
 	public static void main(String[] args) throws Exception {
-		
+
 		EmpreinteMP3 mp = new EmpreinteMP3();
-		System.out.println(mp.empreinteMP3("/net/cremi/rbenojem/espaces/travail/t.mp3"));
-		
+		System.out.println(mp
+				.empreinteMP3("/net/cremi/rbenojem/espaces/travail/t.mp3"));
+
 	}
 
 }
